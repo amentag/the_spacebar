59b3a4ccbd973ff7e4dc2f31d528b116e58a9bf6
developer <mentaga.developer@gmail.com>
2018-12-05 23:05:50 +0100

    use markdown to html

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Michelf\MarkdownInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,10 +20,19 @@ class ArticleController extends AbstractController
     /**
      * @Route("/article/{id}", name="article.show")
      */
-    public function show(int $id)
+    public function show(int $id, MarkdownInterface $markdown)
     {
         return $this->render('article/show.html.twig', [
-            'id' => $id
+            'id' => $id,
+            'content' => $markdown->transform($this->getText())
         ]);
+    }
+
+    protected function getText()
+    {
+        return 'Spicy ***jalapeno*** bacon ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
+                lorem proident **beef** ribs aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
+                labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
+                *turkey* shank eu pork belly meatball non cupim.';
     }
 }
