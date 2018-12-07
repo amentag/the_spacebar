8bf2d7ba6809af00f2893ba89b53b45fc701e252
developer <mentaga.developer@gmail.com>
2018-12-07 11:19:17 +0100

    autowire logger

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 use Michelf\MarkdownInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class MarkdownHelper
@@ -15,11 +16,16 @@ class MarkdownHelper
      * @var MarkdownInterface
      */
     private $markdown;
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
 
-    public function __construct(MarkdownInterface $markdown, AdapterInterface $cache)
+    public function __construct(MarkdownInterface $markdown, AdapterInterface $cache, LoggerInterface $logger)
     {
         $this->cache = $cache;
         $this->markdown = $markdown;
+        $this->logger = $logger;
     }
 
     public function parse(string $content): string
@@ -27,6 +33,7 @@ class MarkdownHelper
         $item = $this->cache->getItem('markdown_' . md5($content));
 
         if (!$item->isHit()) {
+            $this->logger->info('Cache is empty');
             $item->set($this->markdown->transform($content));
             $this->cache->save($item);
         }
